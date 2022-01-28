ea2088b18c8b46e61ed6a6827ae875549234cadf
Bruno Gomes <bruno.gomes.771@outlook.com>
2022-01-28 22:37:56 +0000

    units so, test scene

---
+++ Assets/Scripts/UnitSO.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c11412318ba89f083383520955242e8efa996290720c794b75cec492c029570f
+size 347
---
+++ Assets/Scripts/testUnit.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8f00facafbdcdc985bf017a58f07008f5ad8af9ff9bdb53732f0f7ccf774419
+size 598
