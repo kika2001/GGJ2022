cb119808ab9ae86eebd047fc2691d7303d9ff766
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-28 22:45:10 +0000

    General alpha

---
+++ Assets/Scripts/GeneralPlayer/General.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2eddd7c43434828090eb24caf44d6f782010273069dd2bb2cbcad768cd30c32
+size 2681
---
+++ Assets/Scripts/GeneralPlayer/SimpleMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b647a55621de9956585c4ad273895855ebde926d38ce7e369ee1f14067af6c35
+size 646
