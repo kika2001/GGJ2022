66447f27697aa73120ae0cb9a5c3e4f0fdf2c372
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-29 19:27:41 +0000

    basic settings

--- Assets/Scripts/MenuScripts/MenuScripts.cs
+++ Assets/Scripts/MenuScripts/MenuScripts.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0c661bbd4c7a18aa66c33cba8ba4ea8063281d874b6b672d388abfeb592497a
-size 338
+oid sha256:da56faab3f2690931a4111f0de2c7a5b0ebee006f11a45ce68cdff8851eb4f4e
+size 3191
