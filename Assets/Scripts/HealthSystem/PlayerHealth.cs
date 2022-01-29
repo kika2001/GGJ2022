5397b576b53834a2cf79217cc2a47a7be0159edc
Ricardo Teixeira <kika.teixeira2001@gmail.com>
2022-01-29 19:18:27 +0000

    Health System, Melee And Slayer Sprites

---
+++ Assets/Scripts/HealthSystem/PlayerHealth.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03e8bf262a5c07605540894d8f2c272314d8c9ce7cb0866b8499115f75aea5bb
+size 123
--- Assets/Scripts/Weapons/WeaponHandler.cs
+++ Assets/Scripts/Weapons/WeaponHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c948d9d91d034a54af5fccbb07cb764fd853bf75520b9473fc6c54d21a2f056c
-size 4043
+oid sha256:f356223cd92e352eb3b103a64b9b37c0afb816ec2fd74080b3bd14eef9fd6bb2
+size 4131
